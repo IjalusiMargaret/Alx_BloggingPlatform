676a3dd91dc063b80f6cef907275ed1985560ffe
Ijalusi Margaret <ijalusimargaret@gmail.com>
2025-03-30 21:42:28 +0100

    fix

--- Blogging_Platform/Blogging_Platform/settings.py
+++ Blogging_Platform/Blogging_Platform/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,9 +23,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_n43*va^(-1(_^brs2@@%t7!=921j$crnm0)d(2pfu75to(1sz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['IjalusiMargaret.pythonanywhere.com']
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 
 # Application definition
